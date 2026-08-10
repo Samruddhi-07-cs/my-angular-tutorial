5980c5b012e43870dd3676e26b26189dce6c8358
Samruddhi-07-cs <samruddhishete1215@gmail.com>
2026-08-10 19:53:13 +0530

    update project

--- demo/src/main/java/com/cscorner/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/cscorner/demo/config/SecurityConfig.java
@@ -4,8 +4,8 @@ import com.cscorner.demo.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,12 +23,18 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
+                .cors(cors -> {})
+
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/api/auth/**",
+                                "/api/products/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
 
